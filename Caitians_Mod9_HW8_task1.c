ce1fe12508ee37a18d0279ce8d689438e3da9794
Orlando Salas <orlandosalas@mail.weber.edu>
2017-04-02 01:56:06 +0000

    Initial Commit. Added task1 and task2

---
+++ Caitians_Mod9_HW8_task1.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Caitians_Mod9_HW8_task1.c
+ *       	Usage:  ./Caitians_Mod9_HW8_task1.c
+ *    Description:  comparing strings for their characters
+ *
+ *        Version:  1.0
+ *        Created:  04/02/2017 01:53:42 AM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Orlando Salas (), orlandosalas@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
