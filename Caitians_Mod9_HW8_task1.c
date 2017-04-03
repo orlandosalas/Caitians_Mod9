29d844babe96e72e4cff858c332e0086452448e2
Orlando Salas <orlandosalas@mail.weber.edu>
2017-04-03 04:25:13 +0000

    address successful

--- Caitians_Mod9_HW8_task1.c
+++ Caitians_Mod9_HW8_task1.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		exit(2);
+		//exit(2);
 	fmatch = StringIn( argv[1] , argv[2] ); // ret of func is an address of a func
 	printf("String Test: %s \n", fmatch);
 	}
@@ -73,20 +73,25 @@ char *StringIn(char *string1, char *string2)
 {
 	/* Goal: locate address of string1 match */
 	char *matchAddress = '\0'; // initialize NULL
-	int ssize = strlen( string2 );
+	int s1size = strlen( string1 );
+	int s2size = strlen( string2 );
 	int test2;	
-	//int test2 = strncmp(string
-	while( string1 != 0) // while string 1 is true, repeat
+	//printf("String 1 val: %s\n", string1);
+	//string1 = string1 + 1;
+	//printf("String 1 val: %s\n", string1);
+	//	while( string1) // while string 1 is true, repeat
+//	{
+	for(int i = 0; i < s1size; i++)
 	{
-		test2 = strncmp( string1, string2, ssize);
+		test2 = strncmp( string1, string2, s2size);
 		if( test2 == 0)
 		{
 			matchAddress = string1; // sets the address of match to CharS
+			//printf("\nGood string1 :%s\n", string1);
+			break;
 		}
-		else
-		{
-			string1++;
-		}
+		//printf("\nstring1 :%s\n", string1);
+		string1 = string1 + 1;
 	}
 	//CharS1Address = '\0'; not necessary. You already copied the address of match. Null is included
 
